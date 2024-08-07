9578fa7b1013b77ea80a1859b07e1958229fea14
santosdlaysa <santosdlaysa@gmail.com>
2024-08-07 11:56:37 -0400

    atualizado ajuste no relogio

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,17 +1,17 @@
 import Botao from "../Botao/Botao";
-import style from "./Cronometro.module.scss";
-import Relogio from "./Relogio/index";
+import Relogio from "./Relogio";
+import style from './Cronometro.module.scss';
 
-export default function Cronometro(){
-    return (
-        <div className={style.Cronometro}>
-            <p className={style.titulo}> Escolha um card e inicie o Cronometro</p>
-            <div className={style.relogioWrapper}>
-                <Relogio />
-            </div>
-            <Botao>
-                Começar!
-            </Botao>
-        </div>
-    )
+export default function Cronometro() {
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio />
+      </div>
+      <Botao>
+        Começar!
+      </Botao>
+    </div>
+  )
 }
